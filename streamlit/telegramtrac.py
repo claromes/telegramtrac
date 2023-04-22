cad98b0256eb0a7cd18eee6f983cc67f2689b087
Claromes <claromes@hey.com>
2023-04-22 13:37:04 -0300

    tabs

--- streamlit/telegramtrac.py
+++ streamlit/telegramtrac.py
@@ -1,12 +1,35 @@
 import streamlit as st
+from streamlit_extras.customize_running import center_running
 import subprocess
 import json
 import configparser
+from pandas import read_csv
 
-st.title('telegramtrac')
+#vars
+title_component = st.empty()
+expander_component = st.empty()
+form_component = st.empty()
+sign_in_component = st.empty()
+channel_component = st.empty()
+center_running()
 
-with st.expander('[ ¿ ]'):
-    st.caption("Telegram channel tracker for non-technical researchers.")
+trac = ''
+
+if 'channel_name' not in st.session_state:
+    st.session_state['channel_name'] = 'literaturese'
+
+if 'code_state' not in st.session_state:
+    st.session_state['code_state'] = False
+
+if 'code_value' not in st.session_state:
+    st.session_state['code_value'] = 0
+
+#title
+title_component.title('telegramtrac')
+
+#expander
+with expander_component.expander('[ ¿ ]'):
+    st.caption("Telegram channel tracker for non-programmers.")
     st.divider()
     st.caption('### How to')
     st.caption('[How to create an API ID and API hash](https://core.telegram.org/api/obtaining_api_id)')
@@ -14,22 +37,7 @@ with st.expander('[ ¿ ]'):
     st.caption('### Credits')
     st.caption("telegramtrac by [claromes](https://claromes.gitlab.io) is a fork of DFRLab's Telegram Tracker.")
 
-form_component = st.empty()
-sign_in_component = st.empty()
-channel_component = st.empty()
-json_component = st.empty()
-
-trac = ''
-
-if 'channel_name' not in st.session_state:
-    st.session_state['channel_name'] = ''
-
-if 'code_state' not in st.session_state:
-    st.session_state['code_state'] = False
-
-if 'code_value' not in st.session_state:
-    st.session_state['code_value'] = 1234
-
+#credentials
 with form_component.form(key='config_form'):
     ***REMOVED*** st.text_input('api_id')
     ***REMOVED*** st.text_input('api_hash')
@@ -49,8 +57,10 @@ with form_component.form(key='config_form'):
     send_credentials = st.form_submit_button('send credentials', type='primary')
 
 if send_credentials:
+    center_running()
     st.session_state.code_state = True
 
+#sign in code
 with sign_in_component.form(key='config_sign_in_form'):
     if st.session_state.code_state:
         code_sign_in = st.text_input('code', placeholder="Leave empty and click 'sign in' to receive a sign in code", disabled=False)
@@ -75,10 +85,12 @@ with sign_in_component.form(key='config_sign_in_form'):
         sign_in = st.form_submit_button('sign in', disabled=True, type='primary')
 
     if sign_in:
+        center_running()
         st.session_state.code_value = code_sign_in
 
+#channel name
 with channel_component.form(key='channel_form'):
-    if sign_in and not st.session_state.code_value == 1234:
+    if sign_in and not st.session_state.code_value == 0:
         channel_name = st.text_input('channel name', placeholder="https://t.me/CHANNEL_NAME_IS_HERE", disabled=False, key='channel_name')
         trac = st.form_submit_button('trac', disabled=False, type='primary')
         send_credentials = True
@@ -87,25 +99,68 @@ with channel_component.form(key='channel_form'):
         trac = st.form_submit_button('trac', disabled=True, type='primary')
         send_credentials = True
 
+#data tabs
 if trac:
+    center_running()
+    tab1, tab2, tab3, tab4 = st.tabs(['json', 'dataset', 'metadata', 'new trac'])
+
+    expander_component.empty()
     form_component.empty()
     sign_in_component.empty()
     channel_component.empty()
 
     #SEC ISSUE: New login message not being sent
-    try:
-        cmd = 'python main.py --telegram-channel {}'.format(st.session_state.channel_name)
+    # try:
+    #     cmd = 'python main.py --telegram-channel {}'.format(st.session_state.channel_name)
 
-        output = subprocess.check_output(cmd.split())
-    except subprocess.CalledProcessError as e:
-        pass
-    except Exception as e:
-        pass
+    #     output = subprocess.check_output(cmd.split())
+    # except subprocess.CalledProcessError as e:
+    #     pass
+    # except Exception as e:
+    #     pass
 
-    st.subheader('Messages')
-    json_file = 'output/data/{}/{}_messages.json'.format(st.session_state.channel_name, st.session_state.channel_name)
-    with open(json_file, 'rb') as file:
-        data = json.load(file)
+    #json - main file
+    with tab1:
+        json_file = 'output/data/{}/{}_messages.json'.format(st.session_state.channel_name, st.session_state.channel_name)
+        with open(json_file, 'rb') as file:
+            st.subheader('{} messages (.json)'.format(st.session_state.channel_name))
 
-        json_component.json(data)
-        st.download_button('Download', file_name=json_file, data=file, mime="application/json")
+            data = json.load(file)
+            st.download_button('Download {}_messages.json'.format(st.session_state.channel_name), file_name=json_file, data=file, mime="application/json")
+
+            st.json(data)
+
+    #dataset
+    with tab2:
+        st.subheader('{} dataset (.csv)'.format(st.session_state.channel_name))
+        dataset_csv_file = 'output/data/msgs_dataset.csv'
+
+        with open(dataset_csv_file, 'rb') as file:
+            st.download_button("Download msgs_dataset.csv", file_name=dataset_csv_file, data=file, mime="text/csv")
+            df = read_csv(dataset_csv_file)
+
+            st.dataframe(df)
+
+    #metadata
+    with tab3:
+        metadata_json_file = 'output/data/{}/{}.json'.format(st.session_state.channel_name, st.session_state.channel_name)
+        metadata_txt_file = 'output/data/chats.txt'
+        metadata_chats_csv_file = 'output/data/collected_chats.csv'
+        metadata_counter_csv_file = 'output/data/counter.csv'
+
+        st.subheader('{} metadata'.format(st.session_state.channel_name))
+        with open(metadata_json_file, 'rb') as file:
+            st.download_button('Download {}.json'.format(st.session_state.channel_name), file_name=metadata_json_file, data=file, mime="application/json")
+        with open(metadata_txt_file, 'rb') as file:
+            st.download_button("Download chats.txt", file_name=metadata_txt_file, data=file, mime="text/plain")
+        with open(metadata_chats_csv_file, 'rb') as file:
+            st.download_button("Download collected_chats.csv", file_name=metadata_chats_csv_file, data=file, mime="text/csv")
+        with open(metadata_counter_csv_file, 'rb') as file:
+            st.download_button("Download counter.csv", file_name=metadata_counter_csv_file, data=file, mime="text/csv")
+
+    #restart
+    with tab4:
+        restart = st.button('restart', type='primary')
+
+        if restart:
+            st.experimental_rerun()
